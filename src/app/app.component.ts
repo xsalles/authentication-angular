bcb422862012ac94a35c0aa73bbde374ad49de12
xsalles <xs.salles@gmail.com>
2025-09-29 16:59:55 -0300

    feat: created the input component and removed the redudancy between them

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RightSideComponent } from "./template/right-side/right-side/right-side.component";
-import { PassowrdComponent } from "./components/inputs/input-passowrd/input-passowrd.component";
+import { InputComponent } from "./components/inputs/text/input.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RightSideComponent, PassowrdComponent],
+  imports: [RouterOutlet, RightSideComponent, InputComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'authentication-angular';
+  title = 'Authentication Tivit';
 }
---
+++ src/app/components/inputs/text/input.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+
+@Component({
+  selector: 'component-input',
+  imports: [InputTextModule, FormsModule],
+  templateUrl: './input.component.html',
+  styleUrl: './input.component.scss',
+})
+export class InputComponent {
+  @Input() value: string = '';
+  @Input() label: string = '';
+  @Input() type: string = '';
+}
