f94098875649fe5000d70e606a484ebb5b511a01
xsalles <xs.salles@gmail.com>
2025-09-29 16:31:07 -0300

    feat: created the component input password

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RightSideComponent } from "./template/right-side/right-side/right-side.component";
+import { PassowrdComponent } from "./components/inputs/input-passowrd/input-passowrd.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RightSideComponent],
+  imports: [RouterOutlet, RightSideComponent, PassowrdComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,23 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+        options: {
+          colors: {
+            primary: '#fff',
+          }
+        }
+      },
+    }),
+  ],
 };
