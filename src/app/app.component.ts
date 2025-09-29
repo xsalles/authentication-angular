7a16e0cc345c86b250f20afc219291bfc6907a2a
xsalles <xs.salles@gmail.com>
2025-09-29 15:50:11 -0300

    feat: created the right side template to put in each page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RightSideComponent } from "./template/right-side/right-side/right-side.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RightSideComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/template/right-side/right-side/right-side.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-right-side',
+  imports: [],
+  templateUrl: './right-side.component.html',
+  styleUrl: './right-side.component.scss',
+})
+export class RightSideComponent {}
